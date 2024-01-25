9ffe7b71d89175096e7b129fefafcbb181ebef31
mbarcenas <misael.barcenas@yahoo.com>
2024-01-24 21:18:59 -0600

    completed exercises

--- src/main/java/org/launchcode/controllers/PlanetController.java
+++ src/main/java/org/launchcode/controllers/PlanetController.java
@@ -1,13 +1,17 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.data.Planets;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
 
 @Controller
 public class PlanetController {
-
-    @GetMapping
-    public String displayIndex() {
-        return "index";
+    @GetMapping()
+    public String displayIndex(Model model) {
+        model.addAttribute("planets", Planets.values());
+        return "planets/index";
     }
 }
